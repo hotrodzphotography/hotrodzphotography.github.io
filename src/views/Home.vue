baeb3b30bfe28509b7bf75d321bb8fb9f9710d89
Michael Rodrigues <mrodrig@users.noreply.github.com>
2021-05-09 16:56:13 +0000

    Updates to account for broken APIs, fix #14

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,6 +19,25 @@
             Contact page or by clicking on the email icon in the page footer.
         </p>
 
+        <h2>Background</h2>
+        <p>
+            I became interested in photography several years ago after seeing the big cameras and lenses at my sister's softball tournaments.
+            My parents ended up purchasing me my first DSLR camera - a Canon EOS Digital Rebel XTi with an 18-55mm and 75-300mm lens.
+            I took well over 20,000 photos with that camera and it went practically everywhere.
+        </p>
+        <p>
+            After purchasing my Nissan 370z, I wanted to get better quality photos of it than I could take with just my cell phone.
+            Upon researching the cameras on the market, I decided on a Canon EOS Rebel T5i with an 18-55mm lens.
+            I later purchased a 50mm macro lens to use with it as well.
+            Again, I took over 20,000 photos with it. Many of those photos were taken at various car shows and  cruise nights that I
+            participated in.
+        </p>
+        <p>
+            In search of better quality and more fine-grained control, I recently purchased my first full-frame DSLR camera -
+            a Canon EOS 6D Mark II with the 24-105mm f/4L II lens and Canon battery grip.
+            In the first year of ownership, I have already taken several thousand photos with it and look forward to taking thousands more!
+        </p>
+
         <p>
             Thanks for stopping by!
         </p>
